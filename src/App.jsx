968fb39d721e2ac2eb854aa7cfcf661bf2e9cd77
utku <utkbktss5@gmail.com>
2025-01-05 21:59:04 +0300

    feat(notification):notification new added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import Button from "./components/Button"
 import Accordion from "./components/Accordion";
 const items = [
   {
-    title: 'What is React?',
-    content: 'React is a JavaScript library for building user interfaces.'
+    title: "What is React?",
+    content: "React is a JavaScript library for building user interfaces.",
   },
   {
-    title: 'Why use React?',
-    content: 'React is a favorite among developers due to its simplicity and flexibility.'
+    title: "Why use React?",
+    content:
+      "React is a favorite among developers due to its simplicity and flexibility.",
   },
   {
-    title: 'How do you use React?',
-    content: 'You use React by creating components.'
-  }
+    title: "How do you use React?",
+    content: "You use React by creating components.",
+  },
 ];
 function App() {
   return (
@@ -20,7 +20,7 @@ function App() {
       {/* <Button content="Button" link="/" /> */}
       <Accordion items={items} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
